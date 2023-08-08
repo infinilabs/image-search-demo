abf4430bd4218bb9b91d0c7c1401681d7d60d237
prometheus <984448038@qq.com>
2023-08-08 17:40:33 +0800

    chore: initial commit

---
+++ img_vector.py
@@ -0,0 +1,76 @@
+from img2vec_pytorch import Img2Vec
+from PIL import Image
+from elasticsearch import Elasticsearch
+import io
+
+es = Elasticsearch(["http://localhost:9200"])
+
+
+def convert_rgba_to_rgb(image_path):
+    img = Image.open(image_path)
+
+    if img.mode == 'RGBA':
+        img = img.convert('RGB')
+        img.save(image_path)
+
+def convert_rgba_to_rgb2(image_file):
+    # Open the image file
+    img = Image.open(image_file)
+
+    # Check if the image is in RGBA mode
+    if img.mode == 'RGBA':
+        # Convert the image to RGB mode
+        img = img.convert('RGB')
+
+        # Create a BytesIO object and save the image to it
+        image_io = io.BytesIO()
+        img.save(image_io, format='JPEG')
+
+        # Seek to the beginning of the BytesIO object
+        image_io.seek(0)
+
+        return image_io
+
+    return image_file
+
+def main():
+    img2vec = Img2Vec()
+
+    arr = ["dog.jpg", "dog2.webp", "img.png", "rabbit.png", "cat1.png"]
+    vectors = []
+
+    for img_path in arr:
+        try:
+            img = Image.open(img_path)
+            vec = img2vec.get_vec(img, tensor=True)
+            vec_np = vec.cpu().numpy().flatten().tolist()
+            vectors.append(vec_np)
+        except Exception as e:
+            print(f"Error processing {img_path}: {e}")
+
+    for i, vec in enumerate(vectors):
+        print(f"Vector for {arr[i]} is {vec}")
+        document = {
+            "title": arr[i],
+            "my_vec": vec
+        }
+        es.index(index="img-test", body=document)
+
+
+def vectorization(input):
+    img2vec = Img2Vec()
+    try:
+        img = Image.open(input)
+        vec = img2vec.get_vec(img, tensor=True)
+        vec_np = vec.cpu().numpy().flatten().tolist()
+        print(vec_np)
+        return vec_np
+    except Exception as e:
+        print(f"Error processing image: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    # main()
+    # convert_rgba_to_rgb("rabbit2.png")
+    vectorization("rabbit2.png")
---
+++ vector-demo.py
@@ -0,0 +1,75 @@
+from flask import Flask, request, jsonify, render_template
+from elasticsearch import Elasticsearch
+
+from img_vector import convert_rgba_to_rgb2, vectorization
+
+app = Flask(__name__)
+
+
+@app.route("/search", methods=["POST"])
+def search_service():
+    es = Elasticsearch("http://192.168.3.17:9200")
+
+    # 获取表单数据
+    index_name = request.form.get("index_name")  # 索引名
+
+    # 获取上传的图片文件
+    image_file = request.files.get("image")
+
+    if not index_name or not image_file:
+        return jsonify({"error": "Both index_name and image are required."}), 400
+
+    # 处理图片
+    image0 = convert_rgba_to_rgb2(image_file)
+    vector_arr = vectorization(image0)
+
+    if vector_arr is None:
+        return jsonify({"error": "Error processing image."}), 400
+
+    query_body = {
+        "size": 10,
+        "_source": "title",
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "knn_nearest_neighbors": {
+                            "field": "my_vec",
+                            "vec": {"values": vector_arr},
+                            "model": "lsh",
+                            "similarity": "cosine",
+                            "candidates": 50,
+                        }
+                    }
+                ]
+            }
+        },
+    }
+
+    if not index_name or not vector_arr:
+        return jsonify({"error": "Both index_name and query are required."}), 400
+
+    # 执行搜索
+    response = es.search(index=index_name, body=query_body)
+
+    # 使用模板显示搜索结果
+    # print(response["hits"]["hits"])
+    results = response["hits"]["hits"]
+    print(results)
+    return render_template("search_results.html", results=results)
+
+    # return jsonify(response)  # 返回搜索结果
+
+
+@app.route("/req", methods=["GET"])
+def req_page():
+    return render_template("req.html")
+
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("req.html")
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
